92d6c26256509317f64e5072fd15506f316da37a
Enzo Hugonnier <enzohugonn@gmail.com>
2024-07-06 19:44:10 +0200

    chore(FileZipper): :sparkles: Setup git project

---
+++ FileZipper/FileZipper.cpp
@@ -0,0 +1,12 @@
+﻿// FileZipper.cpp : Defines the entry point for the application.
+//
+
+#include "FileZipper.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
